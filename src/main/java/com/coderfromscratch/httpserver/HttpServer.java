a5638a8ab3a42c8fa1315fd2bcb47c4171f63521
Oussama <you@example.com>
2023-04-05 00:40:09 +0000

    commit message

--- src/main/java/com/coderfromscratch/httpserver/HttpServer.java
+++ src/main/java/com/coderfromscratch/httpserver/HttpServer.java
@@ -18,7 +18,7 @@ public class HttpServer {
     private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
 
     public static void main(String[] args) {
-        System.out.println("heeello");
+        System.out.println("koba na9ch");
 
         LOGGER.info("Server starting...");
 
